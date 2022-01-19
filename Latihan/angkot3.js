ee0860fc6e10ca482618e69ff5e4b96de53f8358
Yoghantara <gyoghantara@gmail.com>
2022-01-19 20:56:33 +0800

    commit on master

--- Latihan/angkot3.js
+++ Latihan/angkot3.js
@@ -5,7 +5,7 @@ var noAngkot = 1;
 for (noAngkot; noAngkot <= jmlAngkot; noAngkot++) {
   if (noAngkot <= 6) {
     console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik.');
-  } else if (noAngkot == 8) {
+  } else if (noAngkot === 8) {
     console.log('Angkot No. ' + noAngkot + ' sedang lembur.');
   } else {
     console.log('Angkot No. ' + noAngkot + ' sedang tidak beroperasi.');
