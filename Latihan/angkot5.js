08095c24551e6d3ddf4601732d3e3d3b5071650c
Yoghantara <gyoghantara@gmail.com>
2022-01-19 21:39:10 +0800

    angkot4 & angkot5 (penggunaan operator logika)

---
+++ Latihan/angkot5.js
@@ -0,0 +1,14 @@
+var jmlAngkot = 10;
+var angkotBeroprasi = 6;
+var noAngkot = 1;
+
+for (noAngkot; noAngkot <= jmlAngkot; noAngkot++) {
+  if (noAngkot <= 6 && noAngkot !== 5) {
+    console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik.');
+  } else if (noAngkot === 5 || noAngkot === 8 || noAngkot === 10) {
+    // cara lebih efektif (memakai logika || (atau))
+    console.log('Angkot No. ' + noAngkot + ' sedang lembur.');
+  } else {
+    console.log('Angkot No. ' + noAngkot + ' sedang tidak beroperasi.');
+  }
+}
