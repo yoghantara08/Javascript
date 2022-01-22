52a08235ae908dca359a48ffa84a885b9359078c
Yoghantara <gyoghantara@gmail.com>
2022-01-22 18:35:11 +0800

    tambah pengulangan

--- Pengulangan dan Pengkondisian bersarang/script.js
+++ Pengulangan dan Pengkondisian bersarang/script.js
@@ -1,8 +1,15 @@
 var s = '';
 
-for (var i = 0; i < 5; i++) {
-  for (var j = 0; j < 5; j++) {
+for (var i = 0; i < 10; i++) {
+  for (var j = 0; j <= i; j++) {
     s += '*';
   }
   s += '\n';
 }
+for (var i = 9; i > 0; i--) {
+  for (var j = 0; j < i; j++) {
+    s += '*';
+  }
+  s += '\n';
+}
+console.log(s);
