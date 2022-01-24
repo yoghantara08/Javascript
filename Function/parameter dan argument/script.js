8cecbf69c9f43c9d6442f9f9bab42845be81b2ce
Yoghantara <gyoghantara@gmail.com>
2022-01-24 18:07:29 +0800

    coba variable arguments (berisi for)

--- Function/parameter dan argument/script.js
+++ Function/parameter dan argument/script.js
@@ -15,13 +15,33 @@
 // var hasil = tambah(a, b);
 // alert(hasil);
 
-function tambah(a, b /* Parameter */) {
-  return a + b;
+// contoh menggunakan 2 function
+// function tambah(a, b /* Parameter */) {
+//   return a + b;
+// }
+
+// function kali(a, b) {
+//   return a * b;
+// }
+
+// var hasil = kali(tambah(2, 3), tambah(5, 4));
+// alert(hasil);
+
+// coba arguments
+// function cobaArguments() {
+//   return arguments;
+// }
+// var coba = cobaArguments(1, 2, 3, 'hi', false);
+// console.log(coba);
+
+// Contoh penggunaan arguments
+function tambah() {
+  var hasil = 0;
+  for (var i = 0; i < arguments.length; i++) {
+    hasil += arguments[i];
+  }
+  return hasil;
 }
 
-function kali(a, b) {
-  return a * b;
-}
-
-var hasil = kali(tambah(2, 3), tambah(5, 4));
-alert(hasil);
+var coba = tambah(5, 5, 10, 15, 20, 25);
+alert(coba);
