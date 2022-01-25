1d6681a714128e0cfd431e4028b49ca45adc72fa
Yoghantara <gyoghantara@gmail.com>
2022-01-25 22:31:40 +0800

    memanipulasi array, menambah,menghapus,menampilkan.

---
+++ Array/manipulasi array/manipulasiArray.js
@@ -0,0 +1,21 @@
+// Manipulasi array
+
+// 1. Menambah isi array
+// var arr = [];
+// arr[0] = [1];
+// arr[2] = [3];
+// arr[3] = [5];
+// arr[6] = [10];
+// console.log(arr);
+
+// 2. Menghapus isi array
+// var arr = [1, 2, 3, 4];
+// arr[1] = undefined;
+// console.log(arr);
+
+// 3. Menampilkan isi array
+var arr = ['gustut', 'ricky', 'galang', 'krisdit', 'rama'];
+
+for (var i = 0; i < arr.length; i++) {
+  console.log('Mahasiswa ke- ' + (i + 1) + ': ' + arr[i]);
+}
