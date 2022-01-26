a3a4465421965c2db7cd0a2aa080af0f8c8df770
Yoghantara <gyoghantara@gmail.com>
2022-01-26 21:10:10 +0800

    menambahkan materi object

---
+++ Object/object.js
@@ -0,0 +1,44 @@
+// OBJECT ADALAH ARRAY YG LEBIH SAKTI
+
+// membuat object
+
+// object literal
+var mhs = {
+  nama: 'Gustut',
+  umur: 20,
+  ips: [3.2, 3.3, 3.65],
+  alamat: {
+    kota: 'Badung',
+    jalan: 'Wahyu Graha',
+  },
+};
+
+// Function declaration
+function buatObjectMahasiswa(nama, umur, ips, alamat) {
+  var mhs = {};
+  mhs.nama = nama;
+  mhs.umur = umur;
+  mhs.ips = ips;
+  mhs.alamat = alamat;
+
+  return mhs;
+}
+var mhs2 = buatObjectMahasiswa('Yogha', 19, [2.5, 3.3, 3.8], 'wahyu graha');
+
+// Constructor
+function Mahasiswa(nama, umur, ips, alamat) {
+  this.nama = nama;
+  this.umur = umur;
+  this.ips = ips;
+  this.alamat = alamat;
+}
+// Memanggilnya harus berisikan "new" didepan constructor (agar JS tidak mengira bahwa constructor tersebut adalah function declaration)
+var mhsConstructor = new Mahasiswa('GusYogha', 21, [3.5, 3.7, 3.9], 'Buduk');
+
+// THIS
+var a = 5;
+// this = window (global scope)
+// this mengembalikan object global
+console.log(this.a);
+
+// jika menggunakan constructor, this mengembalikan object yg baru dibuat
