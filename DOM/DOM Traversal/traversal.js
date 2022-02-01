64033916b7354a9be21cca886931c7cf93b24672
Yoghantara <gyoghantara@gmail.com>
2022-02-01 20:28:36 +0800

    mengganti span close menjadi a dan menambahkan fungsi preventDefault

--- DOM/DOM Traversal/traversal.js
+++ DOM/DOM Traversal/traversal.js
@@ -20,6 +20,7 @@ const close = document.querySelectorAll('.close');
 close.forEach(function (el) {
   el.addEventListener('click', function (e) {
     e.target.parentElement.style.display = 'none';
+    e.preventDefault();
   });
 });
 
