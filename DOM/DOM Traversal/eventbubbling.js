a1f1b91124ccbebf0dabaaf10072843f1a10b54f
Yoghantara <gyoghantara@gmail.com>
2022-02-01 20:51:18 +0800

    event bubbling

---
+++ DOM/DOM Traversal/eventbubbling.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('.container');
+
+container.addEventListener('click', function (e) {
+  if (e.target.className == 'close') {
+    e.target.parentElement.style.display = 'none';
+    e.preventDefault();
+  }
+});
