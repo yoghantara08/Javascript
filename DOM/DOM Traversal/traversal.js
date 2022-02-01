bcdce75af48fc9eed7d12cee5466ae76a974e0ad
Yoghantara <gyoghantara@gmail.com>
2022-02-01 16:35:54 +0800

    mengganti forloop menjadi forEach

--- DOM/DOM Traversal/traversal.js
+++ DOM/DOM Traversal/traversal.js
@@ -9,9 +9,16 @@
 // DOM Traversal
 const close = document.querySelectorAll('.close');
 
-for (let i = 0; i < close.length; i++) {
-  close[i].addEventListener('click', function (e) {
-    // close[i].parentElement.style.display = 'none';
+// for (let i = 0; i < close.length; i++) {
+//   close[i].addEventListener('click', function (e) {
+//     // close[i].parentElement.style.display = 'none';
+//     e.target.parentElement.style.display = 'none';
+//   });
+// }
+
+// forEach (tiap element)
+close.forEach(function (el) {
+  el.addEventListener('click', function (e) {
     e.target.parentElement.style.display = 'none';
   });
-}
+});
