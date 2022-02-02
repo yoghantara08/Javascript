e2e652ffef97a2e5a0dd7ca73d35aece9913d659
Yoghantara <gyoghantara@gmail.com>
2022-02-02 12:53:48 +0800

    membuat script event untuk mengubah src jumbo dengan src thumb

---
+++ Image Gallery/script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('.container');
+const jumbo = document.querySelector('.jumbo');
+
+container.addEventListener('click', function (e) {
+  if (e.target.className == 'thumb') {
+    jumbo.src = e.target.src;
+    e.preventDefault();
+  }
+});
