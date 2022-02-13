e28acb4671d92fb9433e69d50bc861e210b43e28
Yoghantara <gyoghantara@gmail.com>
2022-02-13 12:16:56 +0800

    arrow function

---
+++ Javascript Lanjutan/Arrow Function/arrow-function.js
@@ -0,0 +1,16 @@
+// Arrow Function merupakan bentuk lain yang lebih ringkas dari Function Expression
+
+// Function Expression
+let tampilPesan1 = function (nama) {
+  console.log(`Halo, ${nama}`);
+};
+
+tampilPesan1('Gustut');
+
+// Arrow Function
+// function () menjadi () =>
+let tampilPesan2 = (nama) => {
+  console.log(`Halo, ${nama}`);
+};
+
+tampilPesan2('Yoghantara');
