93c8e48e2472daf74e6b75e6a0f4b4ec11b255df
Yoghantara <gyoghantara@gmail.com>
2022-02-16 10:56:05 +0800

    perulangan for..in

---
+++ For/for-in.js
@@ -0,0 +1,11 @@
+// for..in
+// Creates a loop only iterating over enumerable
+const mhs = {
+  nama: 'Gustut',
+  umur: 20,
+  email: 'tes@gmail.com',
+};
+
+for (m in mhs) {
+  console.log(m, mhs[m]);
+}
