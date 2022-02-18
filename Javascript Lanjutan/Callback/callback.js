c1bb4f94b3f34f2b40564e28d117fca2c510825e
Yoghantara <gyoghantara@gmail.com>
2022-02-18 13:40:25 +0800

    callback (asynchronous)

--- Javascript Lanjutan/Callback/callback.js
+++ Javascript Lanjutan/Callback/callback.js
@@ -47,3 +47,32 @@ const mhs = [
 // console.log(namaMhs);
 
 mhs.forEach((m) => console.log(m.nama));
+
+// ===========================================================
+console.log(' ');
+// Asynchronous Callback
+function getDataMahasiswa(url, success, error) {
+  let xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        success(xhr.response);
+      } else if (xhr.status === 404) {
+        error();
+      }
+    }
+  };
+
+  xhr.open('get', url);
+  xhr.send();
+}
+
+getDataMahasiswa(
+  'data/mahasiswa.json',
+  (results) => {
+    const mhs = JSON.parse(results);
+    mhs.forEach((m) => console.log(m.nama));
+  },
+  () => {}
+);
