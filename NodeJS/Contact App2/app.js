74688f75b9f0be0a4b03c086a01aef5f0058264d
Yoghantara <gyoghantara@gmail.com>
2022-03-06 20:41:54 +0800

    contact app(bagian 3) membuat list contact untuk menampilkan daftar contact

--- NodeJS/Contact App2/app.js
+++ NodeJS/Contact App2/app.js
@@ -1,29 +1,40 @@
 const yargs = require("yargs");
-const { simpanContact } = require("./contacts");
+const contacts = require("./contacts");
 
 // Mengambil argument dari command line
+yargs
+  .command({
+    command: "add",
+    describe: "Menambahkan contact baru",
+    builder: {
+      nama: {
+        describe: "Nama lengkap",
+        demandOption: true,
+        type: "string",
+      },
+      email: {
+        describe: "Email",
+        demandOption: false,
+        type: "string",
+      },
+      noHP: {
+        describe: "Nomor Handphone",
+        demandOption: true,
+        type: "string",
+      },
+    },
+    handler(argv) {
+      contacts.simpanContact(argv.nama, argv.email, argv.noHP);
+    },
+  })
+  .demandCommand();
+
+// Menampilkan daftar contact
 yargs.command({
-  command: "add",
-  describe: "Menambahkan contact baru",
-  builder: {
-    nama: {
-      describe: "Nama lengkap",
-      demandOption: true,
-      type: "string",
-    },
-    email: {
-      describe: "Email",
-      demandOption: false,
-      type: "string",
-    },
-    noHP: {
-      describe: "Nomor Handphone",
-      demandOption: true,
-      type: "string",
-    },
-  },
-  handler(argv) {
-    simpanContact(argv.nama, argv.email, argv.noHP);
+  command: "list",
+  describe: "Menampilkan daftar contact",
+  handler() {
+    contacts.listContact();
   },
 });
 
--- NodeJS/Contact App2/contacts.js
+++ NodeJS/Contact App2/contacts.js
@@ -13,10 +13,16 @@ if (!fs.existsSync(dataPath)) {
   fs.writeFileSync(dataPath, "[]", "utf-8");
 }
 
-const simpanContact = (nama, email, noHP) => {
-  const contact = { nama, email, noHP };
+const loadContact = () => {
   const file = fs.readFileSync("./data/contacts.json", "utf-8");
   const contacts = JSON.parse(file);
+
+  return contacts;
+};
+
+const simpanContact = (nama, email, noHP) => {
+  const contact = { nama, email, noHP };
+  const contacts = loadContact();
 
   // cek duplikat
   const duplikat = contacts.find((contact) => contact.nama === nama);
@@ -47,4 +53,12 @@ const simpanContact = (nama, email, noHP) => {
   console.log(chalk.blue.inverse("Terimakasih sudah mengisi data."));
 };
 
-module.exports = { simpanContact };
+const listContact = () => {
+  const contacts = loadContact();
+  console.log(chalk.cyanBright.bold("Daftar Contact:"));
+  contacts.forEach((contact, i) => {
+    console.log(`${i + 1}. ${contact.nama} - ${contact.noHP}`);
+  });
+};
+
+module.exports = { simpanContact, listContact };
