28af73ebf4c8260f551783ae85f6494c147c41e3
Yoghantara <gyoghantara@gmail.com>
2022-03-06 11:29:39 +0800

    refactoring app di core module (contacts)

--- NodeJS/Core-Module/app2.js
+++ NodeJS/Core-Module/app2.js
@@ -7,16 +7,27 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+// membuat folder data
+const dirPath = "./data";
+if (!fs.existsSync(dirPath)) {
+  fs.mkdirSync(dirPath);
+}
+// membuat file contacts.json jika belum ada
+const dataPath = "./data/contacts.json";
+if (!fs.existsSync(dataPath)) {
+  fs.writeFileSync(dataPath, "[]", "utf-8");
+}
+
 rl.question("Masukkan nama anda: ", (nama) => {
   rl.question("Umur anda: ", (umur) => {
     const contact = { nama, umur };
-    const file = fs.readFileSync("data/contacts.json", "utf-8");
+    const file = fs.readFileSync("./data/contacts.json", "utf-8");
     const contacts = JSON.parse(file);
 
     contacts.push(contact);
 
     try {
-      fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
+      fs.writeFileSync("./data/contacts.json", JSON.stringify(contacts));
     } catch (e) {
       console.log(e);
     }
