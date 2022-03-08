91ed188e56e0a3c0e411ba9b21d6206c5af79dce
Yoghantara <gyoghantara@gmail.com>
2022-03-08 11:23:05 +0800

    nodejs web server, membaca dan menampilkan file html

--- NodeJS/Web Server/app.js
+++ NodeJS/Web Server/app.js
@@ -1,9 +1,38 @@
+const fs = require("fs");
 const http = require("http");
 const port = 3000;
 
-const server = http.createServer((request, respond) => {
-  respond.write("Hello World");
-  respond.end();
+const renderHTML = (path, res) => {
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.write("Error: File not found");
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+};
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+  });
+
+  const url = req.url;
+  switch (url) {
+    case "/about":
+      renderHTML("./about.html", res);
+      break;
+    default:
+      renderHTML("index.html", res);
+      break;
+  }
+  // if (url === "/about") {
+  //   renderHTML("./about.html", res);
+  // } else {
+  //   renderHTML("index.html", res);
+  // }
 });
 
 server.listen(port, () => {
