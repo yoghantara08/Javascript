c950ed54fb8415f76037e9354a5f06240ff4f95d
Yoghantara <gyoghantara@gmail.com>
2022-03-26 20:09:42 +0800

    constructor function

---
+++ Javascript OOP/constructor-function.js
@@ -0,0 +1,12 @@
+// Object biasa
+const mahasiswa = {
+  nama: "Gustut",
+  umur: 20,
+};
+
+// Constructor function
+function Mahasiswa() {}
+
+// membuat object dari constructor function
+const gustut = new Mahasiswa();
+const yoghantara = new Mahasiswa();
