02e1c58390656dbcb75e5cbe7c8818f464fdf24d
Yoghantara <gyoghantara@gmail.com>
2022-03-27 22:20:00 +0800

    super method

--- Javascript OOP/Class/inheritance.js
+++ Javascript OOP/Class/inheritance.js
@@ -1,3 +1,4 @@
+// Parent
 class Employee {
   constructor(name) {
     this.name = name;
@@ -6,8 +7,13 @@ class Employee {
   sayHello(name) {
     console.log(`Hello ${name}, my name is employee ${this.name}`);
   }
+
+  superMethod() {
+    console.log("Ini adalah Super Method");
+  }
 }
 
+// Child
 class Manager extends Employee {
   constructor(name, umur) {
     super(name);
@@ -17,6 +23,11 @@ class Manager extends Employee {
   sayHello(name) {
     console.log(`Hello ${name}, my name is manager ${this.name}`);
   }
+
+  superMethod() {
+    super.superMethod();
+    console.log("Ini adalah Super Method di class child");
+  }
 }
 
 const employee = new Employee("Yoghantara");
@@ -25,3 +36,6 @@ const manager = new Manager("Gustut", 20);
 console.log(manager.name);
 console.log(manager.umur);
 manager.sayHello(employee.name);
+
+employee.superMethod();
+manager.superMethod();
