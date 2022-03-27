62fb15e0831c007d55960a14ef274a064475196f
Yoghantara <gyoghantara@gmail.com>
2022-03-27 19:15:30 +0800

    arrow function

---
+++ React/Refresher/arrow function/script.js
@@ -0,0 +1,25 @@
+// Function biasa
+function printMyName(name) {
+  console.log(name);
+}
+
+printMyName("Gustut");
+
+// Arrow Function
+const consoleMyName = (name) => {
+  console.log(name);
+};
+consoleMyName("Yoghantara");
+
+const sayHello = () => {
+  console.log("Hello");
+};
+sayHello();
+
+const multiply = (number) => {
+  return number * 2;
+};
+console.log(multiply(5));
+
+const tambah = (a, b) => a + b;
+console.log(tambah(50, 20));
