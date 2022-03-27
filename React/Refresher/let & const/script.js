5f17f7ab8856a983b54f4f521d8d38dba1ad0b78
Yoghantara <gyoghantara@gmail.com>
2022-03-27 19:07:56 +0800

    let & const

---
+++ React/Refresher/let & const/script.js
@@ -0,0 +1,20 @@
+//  VAR
+var varName = "gustut";
+console.log(varName); // gustut
+
+varName = "tutgus";
+console.log(varName); // tutgus
+
+// LET
+let letName = "yoghantara";
+console.log(letName); // yoghantara
+
+letName = "gus yogha";
+console.log(letName); // gus yogha
+
+// CONST
+const constName = "ricky";
+console.log(constName); // ricky
+
+constName = "rama"; // error
+console.log(constName);
