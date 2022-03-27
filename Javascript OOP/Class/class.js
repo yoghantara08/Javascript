0f419bb6879ce8e8227a7e0f4ae048094d266899
Yoghantara <gyoghantara@gmail.com>
2022-03-27 21:42:20 +0800

    constructor dan method pada class

--- Javascript OOP/Class/class.js
+++ Javascript OOP/Class/class.js
@@ -1,7 +1,25 @@
-class Person {}
+class Person {
+  // constructor
+  constructor(name) {
+    // property
+    this.name = name;
+  }
 
-// juga memiliki prototype
-Person.prototype.sayHello = () => {};
+  // method
+  sayHello() {
+    console.log(`Hello ${this.name}`);
+  }
+}
 
 const person = new Person();
 console.log(person);
+
+const person2 = new Person("Gustut");
+console.log(person2);
+console.log(person2.name);
+person2.sayHello();
+
+// juga memiliki prototype
+// Person.prototype.sayHello = () => {
+//     console.log("Hello")
+// };
