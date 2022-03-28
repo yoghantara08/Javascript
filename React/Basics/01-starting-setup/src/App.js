c62c3af9ed4e094426426215c3e30660fb3871cc
Yoghantara <gyoghantara@gmail.com>
2022-03-28 18:06:25 +0800

    props (passing data) / mirip parameter

--- React/Basics/01-starting-setup/src/App.js
+++ React/Basics/01-starting-setup/src/App.js
@@ -1,10 +1,57 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Eat at restaurant",
+      amount: 12_0000,
+      date: new Date(2021, 3, 20),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: 500_000,
+      date: new Date(2021, 5, 10),
+    },
+    {
+      id: "e3",
+      title: "Buy Computer",
+      amount: 23_200_000,
+      date: new Date(2021, 4, 8),
+    },
+    {
+      id: "e4",
+      title: "Clothes",
+      amount: 12000,
+      date: new Date(2021, 1, 25),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        // props
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
--- React/Basics/01-starting-setup/src/components/ExpenseItem.js
+++ React/Basics/01-starting-setup/src/components/ExpenseItem.js
@@ -1,16 +1,15 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 3, 20);
-  const expenseTitle = "Beli Makan";
-  const expenseAmount = 12_000;
-
+function ExpenseItem(props) {
+  // const expenseDate = new Date(2021, 3, 20);
+  // const expenseTitle = "Beli Makan";
+  // const expenseAmount = 12_000;
   return (
     <div className="expense-item">
-      <div>{expenseDate.toUTCString()}</div>
+      <div className="expense-item__date">{props.date.toUTCString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">Rp {expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">Rp {props.amount}</div>
       </div>
     </div>
   );
