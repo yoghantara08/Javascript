533728176c68b47a4ca1cc9b1cd8042ed8232144
Yoghantara <gyoghantara@gmail.com>
2022-03-28 15:46:52 +0800

    components styling with css

--- React/Basics/01-starting-setup/src/components/ExpenseItem.js
+++ React/Basics/01-starting-setup/src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>21 Maret 2022</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Beli Makan</h2>
-        <div>Rp12.000</div>
+        <div className="expense-item__price">Rp12.000</div>
       </div>
     </div>
   );
