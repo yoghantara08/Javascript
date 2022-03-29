8c10a37b42e4581814b76d0638a0805451bcd0f9
Yoghantara <gyoghantara@gmail.com>
2022-03-29 11:40:23 +0800

    children props

--- React/Basics/01-starting-setup/src/components/Expenses.js
+++ React/Basics/01-starting-setup/src/components/Expenses.js
@@ -1,9 +1,10 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import "./Expenses.css";
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         // props
         title={props.expenses[0].title}
@@ -25,7 +26,7 @@ function Expenses(props) {
         amount={props.expenses[3].amount}
         date={props.expenses[3].date}
       />
-    </div>
+    </Card>
   );
 }
 
