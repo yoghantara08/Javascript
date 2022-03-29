f0a5e5aec739b96530eeca40aef0290cab74a7ad
Yoghantara <gyoghantara@gmail.com>
2022-03-29 12:43:48 +0800

    change function to arrow function

--- React/Basics/01-starting-setup/src/App.js
+++ React/Basics/01-starting-setup/src/App.js
@@ -1,6 +1,6 @@
 import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -34,6 +34,6 @@ function App() {
       <Expenses expenses={expenses} />
     </div>
   );
-}
+};
 
 export default App;
--- React/Basics/01-starting-setup/src/components/UI/Card.js
+++ React/Basics/01-starting-setup/src/components/UI/Card.js
@@ -1,12 +1,12 @@
 import "./Card.css";
 
-function Card(props) {
+const Card = (props) => {
   const classes = "card " + props.className;
   // props.children
   // berfungsi untuk memparsing data JSX yang mengimport component ini
   // atau bisa disebut component ini berguna sebagai Wrapper
   // agar kita tidak mengulan CSS yang sama berulang-ulang
   return <div className={classes}>{props.children}</div>;
-}
+};
 
 export default Card;
