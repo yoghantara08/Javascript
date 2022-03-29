c4a4a656d8c8aca837d6ca7d9dd93e4e762fe594
Yoghantara <gyoghantara@gmail.com>
2022-03-29 15:54:21 +0800

    Working with State

--- React/Basics/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ React/Basics/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,24 @@
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  // useState mereturn array
+  // yg pertama valuenya sendiri, yg kedua update yg kita inginkan
+  const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
-    console.log("click");
+    setTitle("Title Updated");
+    console.log(title); // masih mengambil data lama dan belum terupdate
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">Rp {props.amount}</div>
       </div>
       {/* function jangan diisi () agar tidak langsung di eksekusi */}
