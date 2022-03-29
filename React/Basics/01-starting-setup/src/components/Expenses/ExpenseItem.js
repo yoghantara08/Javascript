a6492b354fefd26c0e180ed64fdcab6dded164be
Yoghantara <gyoghantara@gmail.com>
2022-03-29 15:34:00 +0800

    Listening to events

--- React/Basics/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ React/Basics/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,12 @@
 import ExpenseDate from "./ExpenseDate";
-import Card from "./UI/Card";
+import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("click");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +14,8 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">Rp {props.amount}</div>
       </div>
+      {/* function jangan diisi () agar tidak langsung di eksekusi */}
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
