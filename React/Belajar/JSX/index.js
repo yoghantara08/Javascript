5df5658bbfb611b5a95e33e3a9e1077fc503ea13
Yoghantara <gyoghantara@gmail.com>
2022-03-31 21:56:39 +0800

    JSX

--- React/Belajar/JSX/index.js
+++ React/Belajar/JSX/index.js
@@ -1,11 +1,18 @@
+const nama = "Gustut";
 const Element = () => {
   return (
-    <ul>
-      <li>Apel</li>
-      <li>Tomat</li>
-      <li>Coklat</li>
-      <li>Jeruk</li>
-    </ul>
+    <div>
+      <ul>
+        <li className="heading-1">{nama}</li>
+        <li>Tomat</li>
+        <li>Coklat</li>
+        <li>Jeruk</li>
+      </ul>
+      <img
+        src="https://images.unsplash.com/photo-1648688356431-00e58d0149fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=760&q=80"
+        alt=""
+      />
+    </div>
   );
 };
 
