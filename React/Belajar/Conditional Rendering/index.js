84532850588feb9f45bb0c87fe84a5a390c9384f
Yoghantara <gyoghantara@gmail.com>
2022-04-02 21:12:07 +0800

    Preventing Component from Rendering

--- React/Belajar/Conditional Rendering/index.js
+++ React/Belajar/Conditional Rendering/index.js
@@ -82,6 +82,38 @@ function Ternary() {
   );
 }
 
+// Preventing Component from Rendering
+function WarningBanner(props) {
+  if (!props.warn) {
+    return null;
+  }
+
+  return <div className="warning">Warning!</div>;
+}
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { showWarning: true };
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  handleToggleClick() {
+    this.setState((state) => ({ showWarning: !state.showWarning }));
+  }
+
+  render() {
+    return (
+      <div>
+        <WarningBanner warn={this.state.showWarning} />
+        <button onClick={this.handleToggleClick}>
+          {this.state.showWarning ? "Hide" : "Show"}
+        </button>
+      </div>
+    );
+  }
+}
+
 function App() {
   return (
     <div>
@@ -96,6 +128,9 @@ function App() {
       <hr />
       <h4>Inline If-Else with Conditional Operator:</h4>
       <Ternary />
+      <hr />
+      <h4>Preventing Component from Rendering:</h4>
+      <Page />
     </div>
   );
 }
