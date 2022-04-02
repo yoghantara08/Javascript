9ddaa1c98419ec8a04c4be208cf2cb8ae096f9d1
Yoghantara <gyoghantara@gmail.com>
2022-04-02 22:52:04 +0800

    babel preset

---
+++ React/Belajar/Babel/public/index.js
@@ -0,0 +1,6 @@
+function App() {
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Hello World"), /*#__PURE__*/React.createElement("p", null, "Menggunakan Babel Production dan preset2nya"));
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render( /*#__PURE__*/React.createElement(App, null));
---
+++ React/Belajar/Babel/src/index.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>Menggunakan Babel Production dan preset2nya</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
