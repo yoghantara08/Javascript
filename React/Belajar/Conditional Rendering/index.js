cccdbcbe3dd56411f3836a81fbb6e8e38ee2153e
Yoghantara <gyoghantara@gmail.com>
2022-04-02 20:57:52 +0800

    Element Variables

--- React/Belajar/Conditional Rendering/index.js
+++ React/Belajar/Conditional Rendering/index.js
@@ -15,11 +15,57 @@ function Greeting(props) {
   return <GuestGreeting />;
 }
 
+// Element Variables
+function LoginButton(props) {
+  return <button onClick={props.onClick}>Login</button>;
+}
+
+function LogoutButton(props) {
+  return <button onClick={props.onClick}>Logout</button>;
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = { isLoggedIn: false };
+  }
+
+  handleLoginClick() {
+    this.setState({ isLoggedIn: true });
+  }
+
+  handleLogoutClick() {
+    this.setState({ isLoggedIn: false });
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+
+    return (
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    );
+  }
+}
+
 function App() {
   return (
     <div>
       <h4>If Statement:</h4>
       <Greeting isLoggedIn={true} />
+      <hr />
+      <h4>Element Variables:</h4>
+      <LoginControl />
     </div>
   );
 }
