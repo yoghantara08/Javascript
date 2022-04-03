4d9b3d2179bdf15574da2aa926f597c223a9b98d
Yoghantara <gyoghantara@gmail.com>
2022-04-03 20:30:32 +0800

    conversion functions

--- React/Belajar/Lifting State Up/index.js
+++ React/Belajar/Lifting State Up/index.js
@@ -59,6 +59,25 @@ class TemperatureInput extends React.Component {
   }
 }
 
+// Writing Conversion Functions
+function toCelsius(fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return "";
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
 function App() {
   return (
     <div>
