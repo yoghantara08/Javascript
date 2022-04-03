6ea930e475de9332ecb6c13d99660333beacc19f
Yoghantara <gyoghantara@gmail.com>
2022-04-03 11:25:23 +0800

    text area tag

--- React/Belajar/Forms/index.js
+++ React/Belajar/Forms/index.js
@@ -45,12 +45,50 @@ class NameForm extends React.Component {
   }
 }
 
+// 2. The textarea Tag
+class EssayForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "Hello there, this is some text in a text area" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert(`${this.state.value}`);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Essay: <br />
+          <textarea
+            value={this.state.value}
+            onChange={this.handleChange}
+          ></textarea>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
 function App() {
   return (
     <div>
       <h1>Forms</h1>
       <h4>Controlled Components:</h4>
       <NameForm />
+      <hr />
+      <h4>The textarea Tag:</h4>
+      <EssayForm />
     </div>
   );
 }
