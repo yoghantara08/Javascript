ca8c58e01fb6504a8450c950f6e15a979f9ea649
Yoghantara <gyoghantara@gmail.com>
2022-04-04 19:56:00 +0800

    Specialization

--- React/Belajar/Composition vs Inheritance/index.js
+++ React/Belajar/Composition vs Inheritance/index.js
@@ -33,12 +33,58 @@ function SplitPane(props) {
   );
 }
 
+// 2. Specialization
+function Dialog(props) {
+  return (
+    <FancyBorder color="blue">
+      <h1 className="Dialog-title">{props.title}</h1>
+      <p className="Dialog-message">{props.message}</p>
+      {props.children}
+    </FancyBorder>
+  );
+}
+
+function WelcomeDialog2() {
+  return (
+    <Dialog title="Welcome" message="Thank you for visiting our spacecraft!" />
+  );
+}
+
+class SignUpDialog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { login: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ login: event.target.value });
+  }
+
+  handleSignUp() {
+    alert(`Welcome aboard, ${this.state.login}`);
+  }
+
+  render() {
+    return (
+      <Dialog title="Avenger Movie" message="Peter Parker become superman">
+        <input value={this.state.login} onChange={this.handleChange} />
+        <button onClick={this.handleSignUp}>Sign Me Up!</button>
+      </Dialog>
+    );
+  }
+}
+
 function App() {
   return (
     <div>
       <h1>Composition vs Inheritance</h1>
       <WelcomeDialog />
       <SplitPane left={<Contacts />} right={<Chat />} />
+      <WelcomeDialog2 />
+      <SignUpDialog />
     </div>
   );
 }
