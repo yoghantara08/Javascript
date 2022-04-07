338eb5c17aaffa286ead42b9b07a21f15e441ede
Yoghantara <gyoghantara@gmail.com>
2022-04-07 21:35:12 +0800

    babel lite server

--- React/Belajar/Babel/public/index.js
+++ React/Belajar/Babel/public/index.js
@@ -1,6 +1,15 @@
 function App() {
-  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Hello World"), /*#__PURE__*/React.createElement("p", null, "Menggunakan Babel Production dan preset2nya"));
+  return /*#__PURE__*/ React.createElement(
+    "div",
+    null,
+    /*#__PURE__*/ React.createElement("h1", null, "Hello World"),
+    /*#__PURE__*/ React.createElement(
+      "p",
+      null,
+      "Menggunakan Babel Production dan preset2nya"
+    )
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render( /*#__PURE__*/React.createElement(App, null));
+root.render(/*#__PURE__*/ React.createElement(App, null));
--- React/Belajar/Babel/src/index.js
+++ React/Belajar/Babel/src/index.js
@@ -7,5 +7,5 @@ function App() {
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
