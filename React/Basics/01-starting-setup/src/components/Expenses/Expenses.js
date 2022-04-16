c95bd0b78ad4d359adba5e052cebad0fc4e2b8e7
Yoghantara <gyoghantara@gmail.com>
2022-04-16 19:27:46 +0800

    keys

--- React/Basics/01-starting-setup/src/components/Expenses/Expenses.js
+++ React/Basics/01-starting-setup/src/components/Expenses/Expenses.js
@@ -18,6 +18,7 @@ const Expenses = (props) => {
         <ExpenseFilter filteredYear={filteredYear} onFilter={filterHandler} />
         {props.expenses.map((expense) => (
           <ExpenseItem
+            key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
