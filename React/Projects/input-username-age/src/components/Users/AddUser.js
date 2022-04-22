c7e5225358bcd0427380958a6e64b47c8e958672
Yoghantara <gyoghantara@gmail.com>
2022-04-22 20:46:51 +0800

    Users component

---
+++ React/Projects/input-username-age/src/components/Users/AddUser.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={addUserHandler}>
+      <label htmlFor="username">Username</label>
+      <input type="text" id="username" />
+      <label htmlFor="age">Age (Years)</label>
+      <input type="number" id="age" />
+      <button type="submit">Add User</button>
+    </form>
+  );
+};
+
+export default AddUser;
