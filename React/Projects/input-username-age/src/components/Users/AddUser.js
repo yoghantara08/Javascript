80bfc522e3ddb5897a611bc7c161da5f827b73fe
Yoghantara <gyoghantara@gmail.com>
2022-04-22 21:01:23 +0800

    add card components and styling

--- React/Projects/input-username-age/src/components/Users/AddUser.js
+++ React/Projects/input-username-age/src/components/Users/AddUser.js
@@ -1,18 +1,23 @@
 import React from "react";
 
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+
 const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input type="text" id="username" />
-      <label htmlFor="age">Age (Years)</label>
-      <input type="number" id="age" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" />
+        <label htmlFor="age">Age (Years)</label>
+        <input type="number" id="age" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
