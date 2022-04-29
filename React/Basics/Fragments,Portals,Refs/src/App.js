52bfdcfcd3b84f7495f331043b53c574a7491fb7
Yoghantara <gyoghantara@gmail.com>
2022-04-29 22:13:15 +0800

    React.Fragment <></>

--- React/Basics/Fragments,Portals,Refs/src/App.js
+++ React/Basics/Fragments,Portals,Refs/src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import AddUser from './components/Users/AddUser';
-import UsersList from './components/Users/UsersList';
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
   const [usersList, setUsersList] = useState([]);
@@ -16,10 +16,10 @@ function App() {
   };
 
   return (
-    <div>
+    <React.Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </React.Fragment>
   );
 }
 
--- React/Basics/Fragments,Portals,Refs/src/components/UI/ErrorModal.js
+++ React/Basics/Fragments,Portals,Refs/src/components/UI/ErrorModal.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import Card from './Card';
-import Button from './Button';
-import classes from './ErrorModal.module.css';
+import Card from "./Card";
+import Button from "./Button";
+import classes from "./ErrorModal.module.css";
 
 const ErrorModal = (props) => {
   return (
-    <div>
+    <>
       <div className={classes.backdrop} onClick={props.onConfirm} />
       <Card className={classes.modal}>
         <header className={classes.header}>
@@ -19,7 +19,7 @@ const ErrorModal = (props) => {
           <Button onClick={props.onConfirm}>Okay</Button>
         </footer>
       </Card>
-    </div>
+    </>
   );
 };
 
