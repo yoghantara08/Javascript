ae3dc1356254500ca21a53d2b3dbf7a61c907063
Yoghantara <gyoghantara@gmail.com>
2022-05-05 10:57:43 +0800

    useContext

--- React/Advanced/Effects,Reducer,Context/src/App.js
+++ React/Advanced/Effects,Reducer,Context/src/App.js
@@ -29,8 +29,13 @@ function App() {
   };
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+      }}
+    >
+      <MainHeader />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
